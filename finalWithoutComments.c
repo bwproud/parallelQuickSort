074fa3dcaff4d36ed34cc4d59a58881f5778eaac
Brennan William Proudfoot <bwproud@classroom.cs.unc.edu>
2017-11-25 20:09:46 -0500

    update final

--- finalWithoutComments.c
+++ finalWithoutComments.c
@@ -22,15 +22,12 @@ void printArray(int n){
     printf("[");
     int t =0;
     for(j = 0; j<n; j++){
-        //if(j<10){
           if(t){
             printf(", %f", N[j]);
           }else{
             t=1;
             printf("%f", N[j]);
           }
-          
-        //}
     }
     printf("]\n");
 }
@@ -59,108 +56,6 @@ int cmpfunc (const void * a, const void * b)
     return 0;  
 }
 
-void swap(double *xp, double *yp){
-    double temp = *xp;
-    *xp = *yp;
-    *yp = temp;
-}
-
-// Merges two subarrays of arr[].
-// First subarray is arr[l..m]
-// Second subarray is arr[m+1..r]
-void merge(int l, int m, int r){
-    int i, j, k;
-    int n1 = m - l + 1;
-    int n2 =  r - m;
- 
-    /* create temp arrays */
-    int L[n1], R[n2];
- 
-    /* Copy data to temp arrays L[] and R[] */
-    for (i = 0; i < n1; i++)
-        L[i] = N[l + i];
-    for (j = 0; j < n2; j++)
-        R[j] = N[m + 1+ j];
- 
-    /* Merge the temp arrays back into arr[l..r]*/
-    i = 0; // Initial index of first subarray
-    j = 0; // Initial index of second subarray
-    k = l; // Initial index of merged subarray
-    while (i < n1 && j < n2){
-        if (L[i] <= R[j]){
-            N[k] = L[i];
-            i++;
-        }else{
-            N[k] = R[j];
-            j++;
-        }
-        k++;
-    }
- 
-    /* Copy the remaining elements of L[], if there
-       are any */
-    while (i < n1){
-        N[k] = L[i];
-        i++;
-        k++;
-    }
- 
-    /* Copy the remaining elements of R[], if there
-       are any */
-    while (j < n2){
-        N[k] = R[j];
-        j++;
-        k++;
-    }
-}
-
-/* l is for left index and r is right index of the
-   sub-array of arr to be sorted */
-void mergeSortHelper(int l, int r){
-    if (l < r){
-        int m = l+(r-l)/2;
- 
-        // Sort first and second halves
-        mergeSortHelper(l, m);
-        mergeSortHelper(m+1, r);
- 
-        merge(l, m, r);
-    }
-}
-
-double mergeSort(int n){
-    time_t t1;
-    t1 = clock();
-    
-    mergeSortHelper(0,n-1);
-    
-    time_t t2;
-    t2 = clock();
-    return (double)(t2-t1)/ CLOCKS_PER_SEC; 
-}
-
-double insertionSort(int n){
-    int key, j;
-    time_t t1;
-    t1 = clock();
-
-    for (int i = 1; i < n; i++){
-      key = N[i];
-      j = i-1;
-
-      while (j >= 0 && N[j] > key){
-         N[j+1] = N[j];
-         j--;
-      }
-      N[j+1] = key;
-    }
-
-    time_t t2;
-    t2 = clock();
-    
-    return (double)(t2-t1)/ CLOCKS_PER_SEC;
-}
-
 int partition(int p, int r){
   double key=N[r];
   int i=p-1;
@@ -183,8 +78,8 @@ int partition(int p, int r){
 void quickSortHelper(int p, int r){
     if(p<r){
         int q=partition(p,r);
-		    quickSortHelper(p,q-1);
-    		quickSortHelper(q+1,r);
+	quickSortHelper(p,q-1);
+    	quickSortHelper(q+1,r);
     }    
 }
 
@@ -216,6 +111,7 @@ void insertionSortHelper(int p, int r){
       N[j+1] = key;
     }
 }
+
 void prefixSum(int arr[], int p, int r){
     int i;
     for(i=p+1;i<r+1;i++){
@@ -259,7 +155,6 @@ int parallelPartition(int p, int r){
   int i,j;
   double temp;
 
-//  printf("%d: before first parallel region\n",omp_get_thread_num());
 //  #pragma omp parallel
 //  {
   //  #pragma omp for schedule(static) private(i)
@@ -281,37 +176,10 @@ int parallelPartition(int p, int r){
     }
  // }
 
-//  printf("before less than: [");
-  // for(i=p; i<r+1; i++){
-  //    printf("%d, ", lt[i]);
-  // }
-  // printf("]\n");
-  // printf("before greater than: [");
-  // for(i=p; i<r+1; i++){
-  //    printf("%d, ", gt[i]);
-  // }
-  // printf("]\n");
-  
-  
- // printf("%d: before prefix sum\n",omp_get_thread_num());
 
-  parallelPrefixSum(p,r);
-  //prefixSum(lt, p,r);
-  //prefixSum(gt,p,r);
-
- // printf("%d: after prefix sum\n",omp_get_thread_num());
-  //prefixSum(lt, gt, p, r);
-
-  // printf("after less than: [");
-  // for(i=p; i<r+1; i++){
-  //    printf("%d, ", lt[i]);
-  // }
-  // printf("]\n");
-  // printf("after greater than: [");
-  // for(i=p; i<r+1; i++){
-  //    printf("%d, ", gt[i]);
-  // }
-  // printf("]\n");
+  //parallelPrefixSum(p,r);
+  prefixSum(lt, p,r);
+  prefixSum(gt,p,r);
 
   int pivot = lt[r];
   N[pivot+p]=key;
@@ -330,7 +198,6 @@ int parallelPartition(int p, int r){
     }
  // }  
 
- // printf("%d: after second parallel\n", omp_get_thread_num());
   return pivot+p;
 }
 
@@ -367,33 +234,6 @@ double parallelQuickSort(int n){
     return (double)(t2-t1)/ CLOCKS_PER_SEC;
 }
 
-double selectionSort(int n){
-    int j, min_idx;
-    
-    time_t t1;
-    t1 = clock();
-
-    // One by one move boundary of unsorted subarray
-    for (int i = 0; i < n-1; i++){
-        
-        // Find the minimum element in unsorted array
-        min_idx = i;
-
-        for (j = i+1; j < n; j++){
-            if (N[j] < N[min_idx]){
-                min_idx = j;
-            }  
-        }
-        double temp = N[i];
-        N[i] = N[min_idx];
-        N[min_idx]=temp;
-    }
-    time_t t2;
-    t2 = clock();
-    
-    return (double)(t2-t1)/ CLOCKS_PER_SEC;
-}
-
 int checkArray(int n){
     int j;
     for(j = 0; j<n-1; j++){
@@ -414,15 +254,11 @@ void tester(int n){
 int main(int argc, char * argv[]){
   FILE* fp = fopen("simTimes.csv","w+");
   int len=15;
-  //int len=5;
   int n[] = {10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000,20000,200000,2000000,20000000,133500000};
   int i;
-  srand(getpid());
-  //tester(10);
+//  srand(getpid());
   for(i = 0; i<len; i++){
     fillArrayRandom(n[i]);
-    //printf("Before for array of size %d:\n", n[i]);
-    //printArray(n[i]);
     double t = parallelQuickSort(n[i]);
     printf("%d elements sorted in %f time\n", n[i], t);
     if(checkArray(n[i])==-1){
@@ -430,8 +266,6 @@ int main(int argc, char * argv[]){
     }else{
       printf("SUCCESSFUL SORT\n");
     }
-    //printf("after for array of size %d:", n[i]);
-    //printArray(n[i]);
   }
   fclose(fp);
 } 
